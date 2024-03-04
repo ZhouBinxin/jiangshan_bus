74453a48a3e0dcb83cc6e8d1a722a0a58493a873
zhoubinxin <binxin.zhou@outlook.com>
2024-03-04 21:21:33 +0800

    feat: 完成首页设计

---
+++ README.md
@@ -0,0 +1,13 @@
+## 江山实时公交
+
+该项目是江山实时公交查询接口的Python绑定，参考项目：[https://github.com/wong2/beijing_bus](https://github.com/wong2/beijing_bus)
+
+### 警告
+
+此项目通过调用非公开接口获取数据，use at your own risk.
+
+### Quick Start
+
+* `pip install -r requirements.txt` 安装依赖
+
+项目自带了一个终端中的查询工具作为例子，运行： `python manage.py cli`
---
+++ page.py
@@ -0,0 +1,36 @@
+# -*-coding:utf-8-*-
+from collections import defaultdict
+
+from flask import Flask, render_template, request, jsonify
+from jiangshan_bus import JiangshanBus
+
+# 查询示例
+QUERY_EXAMPLE = '查询示例： 从西坝河到将台路口西'
+
+# 用户关注公众号时给他推送一条消息
+ON_FOLLOW_MESSAGE = {
+    'title': '使用说明',
+    'description': '',
+    'picurl': 'http://doora.qiniudn.com/H9v9n.jpg',
+    'url': 'http://t.cn/Rz0J1V6',
+}
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+
+@app.route('/')
+def index():
+    names = defaultdict(list)
+    for line in JiangshanBus.get_all_lines():
+        names[line.type].append(line.name)
+    return render_template('index.html', names=names)
+
+
+@app.route('/lines/<line>')
+def show_bus_line(line):
+    return line
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8484)
